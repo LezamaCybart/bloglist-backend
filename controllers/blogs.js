d6bd6d47e3f64ba12d229d47f805ba80ee8f4ae4
LezamaCybart <lezamacybartmatiasgerman@gmail.com>
2021-12-02 10:22:08 -0300

    expansions step9, blog can only be deleted by its creator

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -37,7 +37,22 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const blogToRemove = await Blog.findById(request.params.id)
+  const tokenUser = await User.findById(decodedToken.id)
+  const blogUser = await User.findById(blogToRemove.user)
+
+  if (tokenUser.id.toString() !== blogUser.id.toString()) {
+    return response.status(401).json({ error: 'blog can only be deleted by creator' })
+  }
+
+  await blogToRemove.remove()
+  //await Blog.findByIdAndRemove(request.params.id)
   response.status(204).end()
 })
 
